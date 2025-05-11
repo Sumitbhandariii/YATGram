42674cc19c11b5af9412ae0a3eb0566e530b754b
et-ness <108986309+et-ness@users.noreply.github.com>
2025-05-11 22:34:37 +0200

    Compile all with NDK 25.2

--- TMessagesProj/jni/tde2e/build-tdlib.sh
+++ TMessagesProj/jni/tde2e/build-tdlib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ANDROID_SDK_ROOT=${1:-SDK}
-ANDROID_NDK_VERSION=${2:-24.0.8215888}
+ANDROID_NDK_VERSION=${2:-25.2.9519653}
 OPENSSL_INSTALL_DIR=${3:-third-party/openssl}
 ANDROID_STL=${4:-c++_static}
 TDLIB_INTERFACE=${5:-Java}
